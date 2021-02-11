cf212057e1970de412f4afd92cf11c9aa42f29ce
Shruti13387 <thakurshruti29@gmail.com>
2021-02-11 10:21:26 +0530

    Added Kafka Producer Twitter Code

--- README.md
+++ README.md
@@ -53,3 +53,66 @@
 		kafka-consumer-groups --bootstrap-server localhost:9092 --group my-second-application --reset-offsets --to-earliest --execute --topic first_topic
 	2. Resetting shift-by
 		kafka-consumer-groups --bootstrap-server localhost:9092 --group my-second-application --reset-offsets -shift-by -2 --execute --topic first_topic
+		
+		
+# Important Configuration for Producer
+
+## Producer Acks Deep Dive
+### acks=0 (no acks) 	
+    1. No response is requested.
+    2. If broker goes offline or an exception happens, we won't know and will lose data
+    3. Useful for data where it's okay to potentially lose messages:
+        ex: Matrics collection, Log Collection
+        
+### acks = 1 (leader acks)  DEFAULT
+    1. Leader response is requested, but there's no guarantee of replication. (Happens in a background)
+    2. If an ack is not received, then the producer may retry.
+    3. If the leader goes down before the replicas have the chance to replicate the data yet, then we'll have a data loss.
+        
+### acks = all (replicas acks)
+    1. Leader + Replicas ack requested
+    2. A bit of latency is added but more safety is there if you don't want to loose data
+    3. Acks=all must be used in conjunctio with min.insync.replicas
+    4. min.insync.replicas can be set at broker or topic level(override).
+    5. min.insync.replicas=2 implies that at least 2 broker that are ISR (Including leader) must respond that they have the data
+    6. That means that if you use replication.factor=3, min.insync.replica=2, and acks equals all, you can only tolerate one broker going down otherwise, the producer will receive an exception on send.
+
+##Producer Retries
+    In case of transient failures, the developers are expected to handle these exceptions, otherwise your data will be lost.
+    Example of transient failure:
+        -NotEnoughReplicasException
+        
+    ==> There is a "retries" setting
+        - defaults to 0 to Kafka Version <=2
+        - defaults to 2147483647 to Kafka Version >=2.1
+        
+    ==> The 'retry.backoff.ms' settings is by default 100ms
+    
+    ### Producer Retries: Warning
+        -   In case of retries, there's a chance that messages will be sent out of order.
+            For example, if a batch has failed to be sent.
+        - So if you rely on key-based ordering, that can be a really big issue,
+         
+    Solution for this is idempotent producers
+## Producer Timeouts
+    - If retries > 0, the producer will not rety for ever, it's bounded by timeout
+    - delivery.timeout.ms = 120000ms == 2 min
+    
+# Idempotent Producer
+##Problem: 
+Producer can introduce duplicate messages in Kafka due to network error
+##Solution:
+In Kafka >= 0.11, you can define a "idempotent producer" which won't introduce duplicate dut to n/w error
+##Enable Idempotent Producer
+    producerProps.put("enable.idempotence", true);
+    
+    Q: What it does?
+    A: Sets below properties by default:
+        -> retries = INteger.MAX_VALUE(2^31-1 = 2147483647)
+        -> max.in.flight.requests = 1 (Kafka == 0.11) or
+        -> max.in.flight.requests = 2 (Kafka >= 1.0 - higher performance & keep ordering)
+        -> asks=all
+        
+ #Producer Compression
+    https://blog.cloudflare.com/squeezing-the-firehose/ 
+        
---
+++ src/main/java/com/poc/twitter/producer/TwitterProducer.java
@@ -0,0 +1,149 @@
+package com.poc.twitter.producer;
+
+import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.Hosts;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class TwitterProducer {
+
+
+    List<String> terms = Lists.newArrayList("kafka", "bitcoin", "India");
+    private String consumerKey = "";
+    private String consumerSecret = "";
+    private String token = "-X0PJ2GDc8QgKFgCYCttJIl11b3wbwM";
+    private String secret = "";
+
+    public TwitterProducer() {
+    }
+
+    public static void main(String[] args) {
+        new TwitterProducer().run();
+    }
+
+    /**
+     * 1. Create Twitter client
+     * 2. Create Kafka Producer
+     * 3. Loop the send tweet to Kafka
+     */
+    private void run() {
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<>(100000);
+
+        /**
+         * 1. Create Twitter Client
+         */
+        Client client = createTwitterClient(msgQueue);
+        client.connect();
+
+        /**
+         * 2. Create Kafka Producer
+         */
+        KafkaProducer<String, String> kafkaProducer = createKafkaProducer();
+
+        /**
+         * Add Shutdown Hook
+         */
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            log.info("Stopping application...");
+            log.info("Shutting down client from twitter... ");
+            client.stop();
+            log.info("closing producer...");
+            kafkaProducer.close();
+            log.info("done!");
+        }));
+
+        /**
+         * Now, msgQueue and eventQueue will now start being filled with messages/events. Read from these queues however you like.
+         */
+        while (!client.isDone()) {
+            String msg = null;
+            try {
+                msg = msgQueue.poll(5, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                client.stop();
+            }
+            if (msg != null) {
+                log.info(msg);
+                kafkaProducer.send(new ProducerRecord<>("twitter_tweets", null, msg), (metadata, e) -> {
+                    if (e != null) {
+                        log.error("Something bad happened", e);
+                    }
+                });
+            }
+        }
+        log.info("End of application");
+    }
+
+    public Client createTwitterClient(BlockingQueue msgQueue) {
+
+        /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
+        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
+        // Optional: set up some followings and track terms
+        // List<Long> followings = Lists.newArrayList(1234L, 566788L);
+        //  hosebirdEndpoint.followings(followings);
+        hosebirdEndpoint.trackTerms(terms);
+
+        // These secrets should be read from a config file
+        Authentication hosebirdAuth = new OAuth1(consumerKey, consumerSecret, token, secret);
+
+        ClientBuilder builder = new ClientBuilder()
+                .name("Hosebird-Client-01")                              // optional: mainly for the logs
+                .hosts(hosebirdHosts)
+                .authentication(hosebirdAuth)
+                .endpoint(hosebirdEndpoint)
+                .processor(new StringDelimitedProcessor(msgQueue));
+
+        return builder.build();
+    }
+
+    public KafkaProducer<String, String> createKafkaProducer() {
+
+        Properties properties = new Properties();
+        String bootStrapServer = "127.0.0.1:9092";
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServer);
+
+        /**
+         * Key and value serializer help producer know what kind of values we are sending to kafka
+         * and how these values will be serialized to bytes
+         */
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        /**
+         * High Throughput producer (at the expence of a bit latency and CPU Usage)
+         */
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024)); // 32 KB batch size
+
+        /**
+         * Create safe Producer
+         */
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
+        return new KafkaProducer<String, String>(properties);
+    }
+}
